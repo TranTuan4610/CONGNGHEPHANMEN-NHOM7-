a35ba8b792dedb90676c40ff046402d5faf32271
tranhuutrong1302 <trongth3384@ut.edu.vn>
2025-01-15 22:25:36 +0700

    forms.py
    Signed-off-by: tranhuutrong1302 <trongth3384@ut.edu.vn>

--- pet_management/pets/forms.py
+++ pet_management/pets/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, Pet
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class PetForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'species', 'date_of_birth', 'description', 'is_available_for_adoption']
